c557115a3380f484f49c51503ee1905badd43364
SUST_songao <2292153637@qq.com>
2020-11-27 22:55:41 +0800

    整型、浮点型数据

---
+++ test_11_27/test_11_27/test.c
@@ -0,0 +1,47 @@
+////#include<stdio.h>
+////int main()
+////{
+////	char a[1000];
+////	int i;
+////	for (i = 0; i < 1000; i++)
+////	{
+////		a[i] = -1 - i;
+////	}
+////	printf("%d", strlen(a));
+////	return 0;
+////}//255
+////#include<stdio.h>
+////unsigned char i = 0;//0~255
+////int main()
+////{
+////	for (i = 0; i <= 255; i++)
+////	{
+////		printf("hello world\n");
+////	}
+////	return 0;//255个hello world
+////}
+//#include<stdio.h>
+////int main()
+////{
+////	int n = 9;
+////	float*pFloat = (float*)&n;
+////	printf("n的值为：%f\n", *pFloat);
+////	*pFloat = 9.0;
+////	printf("num的值为：%d\n", n);
+////	printf("*pFloat的值为：%f\n", *pFloat);
+////	return 0;
+////}
+//#include<stdio.h>
+////int main()
+////{
+////	char ch = 'w';
+////	char*pc = &ch;
+////	*pc = 'w';
+////	return 0;
+////}//指针的简单使用方法
+//int main()
+//{
+//	char*pstr = "hellow world";
+//	printf("%s\n", pstr);
+//	return 0;
+//}
